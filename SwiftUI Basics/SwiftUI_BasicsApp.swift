755514fe384c5e59834f1563c88ebfd8ddac8cb2
Carrington Manyuchi <carrington.manyuchi@standardbank.co.za>
2024-11-25 22:13:57 +0200

    Initial Commit

---
+++ SwiftUI Basics/SwiftUI_BasicsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BasicsApp.swift
+//  SwiftUI Basics
+//
+//  Created by Manyuchi, Carrington C on 2024/11/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BasicsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
